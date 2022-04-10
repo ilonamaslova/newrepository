bcb8221b5ce6d7b2ce290874dd9538b1ed897bc1
ilonamaslova <ilonamaslova269@gmail.com>
2022-04-10 18:12:51 +0300

    dummy test

---
+++ src/test/java/TestUnit.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestUnit {
+
+    @Test
+    public void assertTrueCheck(){
+        Assert.assertTrue(1==1);
+    }
+}
